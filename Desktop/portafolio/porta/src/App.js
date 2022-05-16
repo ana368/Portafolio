d825634ea353a52670cd5850dd0eb32e633c4a1c
ana <anaj81297@gmail.com>
2022-05-16 15:57:58 -0500

    project setup

---
+++ Desktop/portafolio/porta/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+   <h2>folio
+
+   </h2>
+  );
+}
+
+export default App;
